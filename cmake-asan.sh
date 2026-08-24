040fe1afc30208afc760590943fe6aeb7a714029
Chandler Carruth <chandlerc@gmail.com>
2026-08-24 03:15:07 +0000

    fixes

--- cmake-asan.sh
+++ cmake-asan.sh
@@ -4,22 +4,21 @@ DIR=${1:-asan}
 mkdir $DIR
 cd $DIR
 
-export CC=~/bin/clang
-export CXX=~/bin/clang++
+export CC=clang
+export CXX=clang++
 
-export CFLAGS='-O2 -gmlt -fno-omit-frame-pointer -fno-optimize-sibling-calls -Xclang -mdisable-tail-calls'
+export CFLAGS='-no-canonical-prefixes -Os -gmlt -fno-omit-frame-pointer -fno-optimize-sibling-calls -fcolor-diagnostics'
 export CXXFLAGS=$CFLAGS
-export LDFLAGS="-Wl,-rpath=$HOME/lib64 -Wl,-rpath=$HOME/lib"
+export LDFLAGS="-Wl,-rpath=\$ORIGIN/../lib -Wl,-rpath=$HOME/lib"
 
-cmake ../../project/llvm -G Ninja \
-  -DLLVM_ENABLE_PROJECTS="clang;lld;lldb;polly;libcxx;libcxxabi;compiler-rt;openmp;libunwind;parallel-libs" \
-  -DCMAKE_C_FLAGS_DEBUG= \
-  -DCMAKE_CXX_FLAGS_DEBUG= \
-  -DLIBCXX_ABI_UNSTABLE=ON \
+cmake ../../llvm-project/llvm -G Ninja \
+  -DCMAKE_BUILD_TYPE=Debug \
+  -DLLVM_ENABLE_PROJECTS="clang;clang-tools-extra;cross-project-tests;lld;lldb;flang" \
+  -DLLVM_ENABLE_RUNTIMES="compiler-rt;libcxx;libcxxabi;libunwind" \
+  -DRUNTIMES_CMAKE_ARGS="-DLLVM_ENABLE_PER_TARGET_RUNTIME_DIR=OFF;-DLIBCXX_ENABLE_STATIC_ABI_LIBRARY=ON;-DLIBCXX_STATICALLY_LINK_ABI_IN_SHARED_LIBRARY=OFF;-DLIBCXX_STATICALLY_LINK_ABI_IN_STATIC_LIBRARY=ON;-DLIBCXX_USE_COMPILER_RT=ON;-DLIBCXX_HAS_ATOMIC_LIB=OFF;-DLIBCXXABI_ENABLE_STATIC_UNWINDER=ON;-DLIBCXXABI_STATICALLY_LINK_UNWINDER_IN_SHARED_LIBRARY=OFF;-DLIBCXXABI_STATICALLY_LINK_UNWINDER_IN_STATIC_LIBRARY=ON;-DLIBCXXABI_USE_COMPILER_RT=ON;-DLIBCXXABI_USE_LLVM_UNWINDER=ON;-DLIBUNWIND_USE_COMPILER_RT=ON;-DCOMPILER_RT_USE_BUILTINS_LIBRARY=ON;-DCOMPILER_RT_USE_LLVM_UNWINDER=ON;-DSANITIZER_CXX_ABI=libc++;-DSANITIZER_TEST_CXX=libc++" \
+  -DBUILTINS_CMAKE_ARGS="-DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON" \
   -DLLVM_CCACHE_BUILD=ON \
   -DLLVM_ENABLE_ASSERTIONS=ON \
   -DLLVM_ENABLE_LIBCXX=ON \
   -DLLVM_ENABLE_LLD=ON \
-  -DLLVM_INCLUDE_GO_TESTS=OFF \
-  -DLLVM_LIBDIR_SUFFIX=64 \
   -DLLVM_USE_SANITIZER='Address;Undefined'
